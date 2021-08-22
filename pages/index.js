97e2d2bbe61fb9c10fc5ecb708e021d750d8b6dc
faquraeshi <ferdous.quraeshi@monstar-lab.com>
2021-08-22 12:26:17 +0600

    'lifting state up' to the parent component

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [country, setCountry] = useState("KSA") // state is `lifted up` to the parent component
+  
   return (
     <div className={styles.container}>
-      <CountryPicker />
-      <CountryDetails />
+      <CountryPicker country={country} setCountry={setCountry} />
+      <CountryDetails country={country} />
     </div>
   )
 }
 
 
-const CountryPicker = () => {
-  const [country, setCountry] = useState("KSA")
+const CountryPicker = ({country, setCountry}) => {
   return (
     <select value={country} onChange={e => setCountry(e.target.value)}>
       <option value="BD">Bangladesh</option>
@@ -21,6 +22,6 @@ const CountryPicker = () => {
   )
 }
 
-const CountryDetails = () => {
-  return <h1>Country Details</h1>
+const CountryDetails = ({country}) => {
+  return <h1>{country}</h1>
 }
